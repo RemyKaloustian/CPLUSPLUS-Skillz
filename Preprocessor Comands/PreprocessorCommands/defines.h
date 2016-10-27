a3f3f88985b4878a94bdfece9e25859188b6fe8a
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-10-27 10:43:17 +0200

    Adding to string in define

--- Preprocessor Comands/PreprocessorCommands/defines.h
+++ Preprocessor Comands/PreprocessorCommands/defines.h
@@ -1,2 +1,8 @@
 #define dat_int 46
 #define dat_string "wat a string !"
+
+//No need to return, returns automatically the line (here a or b)
+#define getmax(a,b) ((a)>(b)?(a):(b))
+
+//the # converts into a string
+#define to_str(x) #x
--- Preprocessor Comands/PreprocessorCommands/main.cpp
+++ Preprocessor Comands/PreprocessorCommands/main.cpp
@@ -7,9 +7,22 @@ using  std::cout;
 
 int main()
 {
-    cout << "The goal is to see the use of preprocessor commands\n";
+    cout << "The goal is to see the use of preprocessor commands\n\n";
 
+    cout << "Using the defined variables :\n";
     cout << "first define int is " <<  dat_int <<"\n";
+    cout << "The string define is " << dat_string << "\n\n";
+
+
+    cout << "Using the defined functions : \n";
+    //Works with int/int, string/string, float/int
+    cout << getmax(3, 78)<<"\n";
+    cout << getmax("aaa", "uug") << "\n";
+    cout << getmax(100.3, 88) << "\n\n";
+
+    //This won't work, as int and string are not compatible
+   // cout << getmax(88, "uug") << "\n";
+    cout << to_str(65) << "\n";
 
     return 0;
 }
