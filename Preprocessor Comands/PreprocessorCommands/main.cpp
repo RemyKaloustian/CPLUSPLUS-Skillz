09620a89756ee91440c08cdb56856f39c9b22415
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-10-27 11:11:37 +0200

    Testing macros and the pragma message :muscle: :goberserk:

--- Preprocessor Comands/PreprocessorCommands/main.cpp
+++ Preprocessor Comands/PreprocessorCommands/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <string>
 
 #include "defines.h"
 
 //IMPORTANT, NO NAMESPACE
 using  std::cout;
+using std::string;
 
 int main()
 {
+    //Display message on output at compilation
+#ifndef MAMA
+#pragma message("MAMA not defined\n");
+#endif
+
     cout << "The goal is to see the use of preprocessor commands\n\n";
 
     cout << "Using the defined variables :\n";
@@ -22,7 +29,24 @@ int main()
 
     //This won't work, as int and string are not compatible
    // cout << getmax(88, "uug") << "\n";
-    cout << to_str(65) << "\n";
+    cout << to_str(65) << "\n\n";
+    
+    c("Now using the cout defined macro\n");
+    //HOWLY SHIT THIS WORKS
+    c("Im in de programe");
+    c(8788);
+    c(98 + "hehfdj");
+    c("dfdf" + 45);
+    string asses = "asses";
+    c("dem" + asses);
+
+    //Useful macros (who knows !)
+     c( __LINE__);
+     c(__FILE__);
+     c(__DATE__);
+     c(__TIME__);
+
+    //Now with different parameters
 
     return 0;
 }
