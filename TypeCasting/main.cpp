2fb1fa9de9153db38a1d80c9e1b81bc6f7df4202
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-10-29 07:24:25 +0200

    Making the dynamic example work and explaining it :muscle: :heart_eyes:

--- TypeCasting/main.cpp
+++ TypeCasting/main.cpp
@@ -6,23 +6,58 @@
 using std::cout;
 using std::string;
 
-class Base { virtual void dodo(){} };
+//THIS PROGRAM SHOWS THE DIFFERNCE BETWEEN THE CASTING POSSIBILITIES
+
+class Base { public:virtual void dodo(){ showMessage("In dodoodod"); } };
 class Derived :public Base { public : int a = 6; };
 
 int main()
 {
+    #pragma region DYNAMIC
     Base * from_derived = new Derived();
     Base * from_base = new Base();
 
     Derived * cast_result;
 
+    //This cast is going to work, because from_derived is dynamically a Derived, and we put it in another Derived, so no prroblemo
     cast_result = dynamic_cast<Derived*>(from_derived);
-    /*string res = "After a dynamic cast of the Derived object, a = " + cast_result->a->std::to_string();*/
-    //c(cast_result->a);
+    //The a prints fine and dodo() executes fine
+    showMessage("The dynamic cast from derived object has a = ", cast_result->a);
+    cast_result->dodo();
 
-    puttt("datahv");
+    //Now here comes the real deal, this is why we use DYNAMIC cast.
+    //Here, we are trying to put a Base in a Derived, which will NOT work, BECAUSE, Base is NOT A FULL Derived object
+    cast_result = dynamic_cast<Derived*>(from_base);
+    //a will not be printed
+    showMessage("The dynamic cast from base object has a = ", cast_result->a);
+    //This will show because the cast failed 
+    if (cast_result == 0)showMessage("cast failed mah man");
+    //This will cause a runtime exception
+    cast_result->dodo();
+
+    #pragma endregion
+
+#pragma region STATIC
+    Base * from_derived = new Derived();
+    Base * from_base = new Base();
+
+    Derived * cast_result;
+
+    //This cast is going to work, because from_derived is dynamically a Derived, and we put it in another Derived, so no prroblemo
     cast_result = dynamic_cast<Derived*>(from_derived);
+    //The a prints fine and dodo() executes fine
+    showMessage("The dynamic cast from derived object has a = ", cast_result->a);
+    cast_result->dodo();
 
-    showMessage("value1: ", 5, " and value2: ", 'a');
+    //Now here comes the real deal, this is why we use DYNAMIC cast.
+    //Here, we are trying to put a Base in a Derived, which will NOT work, BECAUSE, Base is NOT A FULL Derived object
+    cast_result = dynamic_cast<Derived*>(from_base);
+    //a will not be printed
+    showMessage("The dynamic cast from base object has a = ", cast_result->a);
+    //This will show because the cast failed 
+    if (cast_result == 0)showMessage("cast failed mah man");
+    //This will cause a runtime exception
+    cast_result->dodo();
 
+#pragma endregion
 }
