7e6779f7776c47990e78ad055e55fbd4d90ec1e5
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-10-29 07:54:59 +0200

    Correcting typos mah man :muscle: :heart_eyes_cat:

--- TypeCasting/main.cpp
+++ TypeCasting/main.cpp
@@ -8,9 +8,9 @@
 using std::cout;
 using std::string;
 
-//THIS PROGRAM SHOWS THE DIFFERNCE BETWEEN THE CASTING POSSIBILITIES
+//THIS PROGRAM SHOWS THE CASTING POSSIBILITIES
 
-class Base { public:virtual void dodo(){ showMessage("In dodoodod"); } };
+class Base { public:virtual void dodo(){ showMessage("In dodo() mah man"); } };
 class Derived :public Base { public : int a = 6; };
 
 int main()
@@ -23,7 +23,7 @@ int main()
 
     Derived * cast_result;
 
-    //This cast is going to work, because from_derived is dynamically a Derived, and we put it in another Derived, so no prroblemo
+    //This cast is going to work, because from_derived is dynamically a Derived, and we put it in another Derived, so no problemo
     cast_result = dynamic_cast<Derived*>(from_derived);
     //The a prints fine and dodo() executes fine
     showMessage("The dynamic cast from derived object has a = ", cast_result->a);
@@ -61,14 +61,15 @@ int main()
     //This WILL work. It won't throw an exception outta nowhere.
 
     //THIS is a COMPLETE different approach than the dynamic, In short:
-    //Dynamic is like: "Ok, I want to cast something but I want to be SURE that the result object is VALID, so throw an exception if the cast does not work, because we want VALID data.
+    //Dynamic is like: "Ok, I want to cast something but I want to be SURE that the result object is VALID, 
+    //so throw an exception if the cast does not work, because we want VALID data.
     //Static is like: Lol I don't care, I won't throw an exception I promise. You can cast anything into anything, some things might work, some might not, it's up to you.
     cast_result = static_cast<Derived*>(from_base);
-    //a will BE printed, but with a, INVALID value
+    //a will BE printed, but with an INVALID value !
     showMessage("The dynamic cast from base object has a = ", cast_result->a);
-    //This will NOT show because static cast makes all casts work, it's just that the data does not automatically follow 
+    //This will NOT show because static cast makes all casts work, it's just that the data does not automatically follow. 
     if (cast_result == 0)showMessage("cast failed mah man");
-    //This will work. cast_result has been cast to Base, it still can execute dodo()
+    //This will work. cast_result has been cast to Base, it still can execute dodo().
     cast_result->dodo();
     #endif
 #pragma endregion
